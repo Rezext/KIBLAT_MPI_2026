9ad01f8ca625b50be89fbaff88c566f641795874
Rezext <officialrezext@gmail.com>
2025-11-24 16:52:11 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,10 @@
-// Import Firebase modules
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
-import { getStorage } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js';
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js';
-
-// Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBsUD_rxmmW5BCFK37k_0LxF1RQzYsQThI",
-    authDomain: "kiblat-mpi-2026.firebaseapp.com",
-    projectId: "kiblat-mpi-2026",
-    storageBucket: "kiblat-mpi-2026.firebasestorage.app",
-    messagingSenderId: "466657139642",
-    appId: "1:466657139642:web:6981463f755ecb1b82db83",
-    measurementId: "G-D77ENTXG61"
+  apiKey: "AIzaSyBsUD_rxmmW5BCFK37k_0LxF1RQzYsQThI",
+  authDomain: "kiblat-mpi-2026.firebaseapp.com",
+  projectId: "kiblat-mpi-2026",
+  storageBucket: "kiblat-mpi-2026.firebasestorage.app",
+  messagingSenderId: "466657139642",
+  appId: "1:466657139642:web:6981463f755ecb1b82db83",
+  measurementId: "G-D77ENTXG61"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
-const analytics = getAnalytics(app);
-
-// Export untuk digunakan di script.js
-export { db, storage, analytics };
