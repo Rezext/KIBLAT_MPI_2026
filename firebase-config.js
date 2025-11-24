0eeec77066ac24d7f2a04414ffc9f05fd98426ac
Rezext <officialrezext@gmail.com>
2025-11-24 16:54:31 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,7 @@
+// Import Firebase modules
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBsUD_rxmmW5BCFK37k_0LxF1RQzYsQThI",
@@ -8,3 +12,11 @@ const firebaseConfig = {
   appId: "1:466657139642:web:6981463f755ecb1b82db83",
   measurementId: "G-D77ENTXG61"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Export (storage di-comment dulu)
+export { db };
+// export { db, storage }; // Uncomment kalau sudah upgrade Blaze Plan
